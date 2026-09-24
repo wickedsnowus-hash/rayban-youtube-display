e8a2c95b871ff6e46bb777cb0339f1cbc39da226
wickedsnowus-hash <wickedsnow.us@gmail.com>
2026-09-24 18:59:24 -0500

    Remove unnecessary characters from PLAYLIST_ID

--- scripts/refresh-playlist.js
+++ scripts/refresh-playlist.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var https = require("https");
 var path = require("path");
 
-var PLAYLIST_ID = "PLZgTkNmA_H4I&si=vdD49aN58jK7aUKL";
+var PLAYLIST_ID = "PLZgTkNmA_H4I";
 var FEED_URL = "https://www.youtube.com/feeds/videos.xml?playlist_id=" + encodeURIComponent(PLAYLIST_ID);
 var OUT = path.resolve(__dirname, "..", "playlist.json");
 
